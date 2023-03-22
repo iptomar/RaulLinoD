3a9d6a16289de4c046d05e47d692d47e4f35b020
FelicianoIvan <aluno20482@ipt.pt>
2023-03-22 18:35:36 +0000

    Project Creation
    Project Creation with navigation Bar
    
    Co-Authored-By: AfonsoG2022 <102040695+AfonsoG2022@users.noreply.github.com>
    Co-Authored-By: Dianaleite01 <101589607+Dianaleite01@users.noreply.github.com>
    Co-Authored-By: aluno23032 <101656878+aluno23032@users.noreply.github.com>

---
+++ app/src/main/java/com/example/raul_lino_d/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.raul_lino_d.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
