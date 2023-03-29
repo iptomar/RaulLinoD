4768d5566cf0a5451f4c5dc550317cb42c7b1c30
aluno23032 <aluno23032@ipt.pt>
2023-03-29 18:32:56 +0100

    Adicionar ícones à navbar

--- app/src/main/java/com/example/raul_lino_d/MainActivity.kt
+++ app/src/main/java/com/example/raul_lino_d/MainActivity.kt
@@ -18,9 +18,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val navView: BottomNavigationView = binding.navView
-
+        navView.itemIconTintList = null
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
