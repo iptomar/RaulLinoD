204fd3d528a3bced2336a217a5b9df32774fcd21
aluno23032 <aluno23032@ipt.pt>
2023-04-12 18:54:47 +0100

    Correção de nome de pastas

--- app/src/main/java/com/example/raul_lino_d/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/raul_lino_d/ui/dashboard/DashboardViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.raul_lino_d.ui.home
+package com.example.raul_lino_d.ui.dashboard
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
