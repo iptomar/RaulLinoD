c99b23ac203371a3613ee98225db6a4638cd54b5
aluno23032 <aluno23032@ipt.pt>
2023-04-26 20:45:48 +0100

    Ainda n funciona
    Co-Authored-By: Dianaleite01 <101589607+Dianaleite01@users.noreply.github.com>

--- app/src/main/java/com/example/raul_lino_d/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/raul_lino_d/ui/map/MapFragment.kt
@@ -1,17 +1,22 @@
 package com.example.raul_lino_d.ui.map
 
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.raul_lino_d.MainActivity
 import com.example.raul_lino_d.R
 import com.example.raul_lino_d.databinding.FragmentMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
@@ -24,55 +29,63 @@ class MapFragment : Fragment() {
 
     private lateinit var parent: MainActivity
     private var _binding: FragmentMapBinding? = null
-
-
-
-
     private lateinit var map : MapView
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
-
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val mapViewModel =
-                ViewModelProvider(this).get(MapViewModel::class.java)
-
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        parent = (activity as MainActivity).getMain()
-
+        parent = activity as MainActivity
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(parent)
+        fetchLocation()
         Configuration.getInstance().userAgentValue = parent.getPackageName()
         map = root.findViewById(R.id.map)
         map.setTileSource(TileSourceFactory.MAPNIK)
         map.controller.zoomTo(17.0)
         map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.ALWAYS)
         map.setMultiTouchControls(true) // para poder fazer zoom com os dedos
-        var compassOverlay = CompassOverlay(parent, map)
+        val compassOverlay = CompassOverlay(parent, map)
         compassOverlay.enableCompass()
         map.overlays.add(compassOverlay)
-        var point = GeoPoint(39.60068, -8.38967)
-        var startMarker = Marker(map)
+        val point = GeoPoint(39.60068, -8.38967)
+        val startMarker = Marker(map)
         startMarker.position = point
         startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
         map.overlays.add(startMarker)
         Handler(Looper.getMainLooper()).postDelayed({
             map.controller.setCenter(point)
         }, 1000) // espera 1 Segundo para centrar o mapa
-
-
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun fetchLocation() {
+        val task = fusedLocationProviderClient.lastLocation
+        if(ActivityCompat.checkSelfPermission(parent, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(parent, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(parent, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            return
+        }
+        task.addOnSuccessListener {
+            if(it != null) {
+                Log.e("Lat", it.latitude.toString())
+            } else {
+                Log.e("teste", "123")
+            }
+        }
     }
 }
